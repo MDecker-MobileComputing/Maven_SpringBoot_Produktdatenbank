211ec49eafa358b0628916f490cf1f1b6ec08a61
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-17 16:19:41 +0100

    Projekt von Spring Initializr erzeugt https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.0&packaging=jar&jvmVersion=23&groupId=de.eldecker.dhbw.spring&artifactId=produktdatenbank&name=Produktdatenbank&description=Rechner%C3%BCbung%20f%C3%BCr%20Thymeleaf-Templates&packageName=de.eldecker.dhbw.spring.produktdatenbank&dependencies=thymeleaf,web

---
+++ src/main/java/de/eldecker/dhbw/spring/produktdatenbank/ProduktdatenbankApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.produktdatenbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduktdatenbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduktdatenbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/produktdatenbank/ProduktdatenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.produktdatenbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduktdatenbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
