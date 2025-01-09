07cd4bc5cfb196a791a299034b71616c6a59c115
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-01-09 14:11:09 +0100

    Fix fuer Option in maven_javadoc.bat+sh

--- maven_javadoc.sh
+++ maven_javadoc.sh
@@ -2,7 +2,7 @@
 
 clear
 
-./mvnw clean javadoc:javadoc -Dshop=private -DadditionalJOption=-Xdoclint:none
+./mvnw clean javadoc:javadoc -Dshow=private -DadditionalJOption=-Xdoclint:none
 # -Xdoclint:none: Keine Warnung, wenn für eine Methode/Argument/Klasse kein JavaDoc-Kommentar angegeben
 
 echo
